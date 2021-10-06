789460cb142d95a7d03a4fcb5c9c4c3ac1017ecb
benjamincgabriel <benjamincgabriel@gmail.com>
2021-10-06 12:19:41 -0400

    Added basic data storage and renamed test.py to test_interface.py
    renamed to make room for another python file add scrolling functionality

--- test_interface.py
+++ test_interface.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
+import time
+
+with open('sens.txt','r') as f:
+    contents = f.readlines()
+
+contents[0] = contents[0][:len(contents[0]) - 1]
 
 root = tk.Tk()
 root.geometry('600x250')
@@ -10,11 +16,14 @@ root.title('Slider')
 current_value1 = tk.DoubleVar()
 current_value2 = tk.DoubleVar()
 
+current_value1.set(contents[0])
+current_value2.set(contents[1])
+
 def get_current_value1():
-    return '{: .0f}'.format(current_value1.get())
+    return '{:.0f}'.format(current_value1.get())
 
 def get_current_value2():
-    return '{: .0f}'.format(current_value2.get())
+    return '{:.0f}'.format(current_value2.get())
 
 value_label1 = ttk.Label(root, text=get_current_value1())
 value_label1.place(relx=0.25, rely=.5, anchor="center")
@@ -24,9 +33,16 @@ value_label2.place(relx=0.75, rely=.5, anchor="center")
 
 def slider_changed1(event):
     value_label1.configure(text=get_current_value1())
+    contents[0] = get_current_value1()
+    with open('sens.txt','w') as f:
+        f.writelines([contents[0] + "\n", contents[1]])
+
 
 def slider_changed2(event):
     value_label2.configure(text=get_current_value2())
+    contents[1] = get_current_value2()
+    with open('sens.txt','w') as f:
+        f.writelines([contents[0] + "\n", contents[1]])
 
 slider1 = ttk.Scale(
     root,
